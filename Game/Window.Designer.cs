4476d88616505d493837054bada4ea2b8175639c
ThunderGemios10 <thundergemios10@gmail.com>
2016-04-29 00:43:32 +0800

    Initial commit

---
+++ Game/Window.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Game
+{
+    partial class Window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Window));
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pad1 = new System.Windows.Forms.PictureBox();
+            this.pad2 = new System.Windows.Forms.PictureBox();
+            this.pad3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.ScoreLabel = new System.Windows.Forms.Label();
+            this.gameClock = new System.Windows.Forms.Timer(this.components);
+            this.moveObjects = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pad1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pad2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pad3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox2.BackgroundImage = global::Game.Properties.Resources.border_bottom;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 602);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1267, 25);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox1.BackgroundImage = global::Game.Properties.Resources.border_top;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1267, 25);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pad1
+            // 
+            this.pad1.BackgroundImage = global::Game.Properties.Resources.tile_1_a;
+            this.pad1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pad1.Location = new System.Drawing.Point(481, 583);
+            this.pad1.Name = "pad1";
+            this.pad1.Size = new System.Drawing.Size(89, 44);
+            this.pad1.TabIndex = 6;
+            this.pad1.TabStop = false;
+            // 
+            // pad2
+            // 
+            this.pad2.BackgroundImage = global::Game.Properties.Resources.tile_2_a;
+            this.pad2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pad2.Location = new System.Drawing.Point(576, 583);
+            this.pad2.Name = "pad2";
+            this.pad2.Size = new System.Drawing.Size(80, 44);
+            this.pad2.TabIndex = 5;
+            this.pad2.TabStop = false;
+            // 
+            // pad3
+            // 
+            this.pad3.BackgroundImage = global::Game.Properties.Resources.tile_3_a;
+            this.pad3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pad3.Location = new System.Drawing.Point(662, 583);
+            this.pad3.Name = "pad3";
+            this.pad3.Size = new System.Drawing.Size(89, 44);
+            this.pad3.TabIndex = 4;
+            this.pad3.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = global::Game.Properties.Resources.bounds;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(0, 22);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(1267, 580);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox3_MouseDown);
+            this.pictureBox3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox3_MouseUp);
+            // 
+            // ScoreLabel
+            // 
+            this.ScoreLabel.AutoSize = true;
+            this.ScoreLabel.BackColor = System.Drawing.Color.Gray;
+            this.ScoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScoreLabel.Location = new System.Drawing.Point(1112, 44);
+            this.ScoreLabel.Name = "ScoreLabel";
+            this.ScoreLabel.Size = new System.Drawing.Size(18, 20);
+            this.ScoreLabel.TabIndex = 7;
+            this.ScoreLabel.Text = "0";
+            // 
+            // gameClock
+            // 
+            this.gameClock.Interval = 400;
+            this.gameClock.Tick += new System.EventHandler(this.gameClock_Tick);
+            // 
+            // moveObjects
+            // 
+            this.moveObjects.Interval = 1;
+            this.moveObjects.Tick += new System.EventHandler(this.moveObjects_Tick);
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1260, 625);
+            this.Controls.Add(this.ScoreLabel);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pad1);
+            this.Controls.Add(this.pad2);
+            this.Controls.Add(this.pad3);
+            this.Controls.Add(this.pictureBox3);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Window";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Game";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pad1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pad2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pad3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pad3;
+        private System.Windows.Forms.PictureBox pad2;
+        private System.Windows.Forms.PictureBox pad1;
+        private System.Windows.Forms.Label ScoreLabel;
+        private System.Windows.Forms.Timer gameClock;
+        private System.Windows.Forms.Timer moveObjects;
+    }
+}
+
---
+++ Game/Window.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+    public partial class Window : Form
+    {
+        public Window()
+        {
+            InitializeComponent();
+            if (Screen.PrimaryScreen.Bounds.Width < 1366 && Screen.PrimaryScreen.Bounds.Height < 768)
+            {
+                MessageBox.Show("Your resolution is required to be at minimum 1366x768 (768p).");
+                Environment.Exit(0);
+            }
+            else
+            {
+                gameClock.Start();
+                moveObjects.Start();
+            }
+        }
+
+        Point pad1a = new Point(481, 583);
+        Point pad2a = new Point(576, 583);
+        Point pad3a = new Point(662, 583);
+
+        Point pad1b = new Point(481, 563);
+        Point pad2b = new Point(576, 563);
+        Point pad3b = new Point(662, 563);
+
+        Point spawn1 = new Point(481, -50);
+        Point spawn2 = new Point(576, -50);
+        Point spawn3 = new Point(662, -50);
+
+        Point dummySpawn = new Point(500, 500);
+
+        Dictionary<int, PictureBox> entities = new Dictionary<int, PictureBox>();
+
+        private void pictureBox3_MouseDown(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    pad1.Location = pad1b;
+                    foreach (var key in entities.Keys.ToList())
+                    {
+                        if (pad1.Bounds.IntersectsWith(entities[key].Bounds))
+                        {
+                            Controls.Remove(entities[key]);
+                            entities.Remove(key);
+                            ScoreLabel.Text = (int.Parse(ScoreLabel.Text) + 100).ToString();
+                            Stream str = Properties.Resources.Pad1;
+                            SoundPlayer snd = new SoundPlayer(str);
+                            snd.Play();
+                        }
+                    }
+                }
+                else if (e.Button == MouseButtons.Middle)
+                {
+                    pad2.Location = pad2b;
+                    foreach (var key in entities.Keys.ToList())
+                    {
+                        if (pad2.Bounds.IntersectsWith(entities[key].Bounds))
+                        {
+                            Controls.Remove(entities[key]);
+                            entities.Remove(key);
+                            ScoreLabel.Text = (int.Parse(ScoreLabel.Text) + 100).ToString();
+                            Stream str = Properties.Resources.Pad2;
+                            SoundPlayer snd = new SoundPlayer(str);
+                            snd.Play();
+                        }
+                    }
+                }
+                else if (e.Button == MouseButtons.Right)
+                {
+                    pad3.Location = pad3b;
+                    foreach (var key in entities.Keys.ToList())
+                    {
+                        if (pad3.Bounds.IntersectsWith(entities[key].Bounds))
+                        {
+                            Controls.Remove(entities[key]);
+                            entities.Remove(key);
+                            ScoreLabel.Text = (int.Parse(ScoreLabel.Text) + 100).ToString();
+                            Stream str = Properties.Resources.Pad3;
+                            SoundPlayer snd = new SoundPlayer(str);
+                            snd.Play();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                moveObjects.Stop();
+                MessageBox.Show("Your computer cannot keep up with the load, application will now close.");
+                Environment.Exit(0);
+                return;
+            }
+        }
+
+        private void pictureBox3_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                pad1.Location = pad1a;
+            }
+            else if (e.Button == MouseButtons.Middle)
+            {
+                pad2.Location = pad2a;
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                pad3.Location = pad3a;
+            }
+        }
+
+        private void gameClock_Tick(object sender, EventArgs e)
+        {
+            if (entities.Count > 25)
+            {
+                moveObjects.Stop();
+                MessageBox.Show("Your computer cannot keep up with the load, application will now close.");
+                Environment.Exit(0);
+                return;
+            }
+            int pad = new Random().Next(1, 4);
+            PictureBox entity = new PictureBox();
+            entity.BorderStyle = BorderStyle.Fixed3D;
+            if (pad == 1)
+            {
+                entity.Size = new Size(89, 44);
+                entity.Location = spawn1;
+                entity.BackColor = Color.Red;
+            }
+            else if (pad == 2)
+            {
+                entity.Size = new Size(80, 44);
+                entity.Location = spawn2;
+                entity.BackColor = Color.Yellow;
+            }
+            else if (pad == 3)
+            {
+                entity.Size = new Size(89, 44);
+                entity.Location = spawn3;
+                entity.BackColor = Color.Blue;
+            }
+            try
+            {
+                foreach (var key in entities.Keys.ToList())
+                {
+                    if (entities[key].Bounds.IntersectsWith(entity.Bounds))
+                    {
+                        return;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            Controls.Add(entity);
+            pictureBox3.SendToBack();
+            entities.Add(new Random().Next(), entity);
+        }
+
+        private void moveEntities_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void moveObjects_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (var key in entities.Keys.ToList())
+                {
+                    if (entities.ContainsKey(key))
+                    {
+                        if (entities[key].Location.Y > Height)
+                        {
+                            Controls.Remove(entities[key]);
+                            entities.Remove(key);
+                            return;
+                        }
+                        entities[key].Location = new Point(entities[key].Location.X, entities[key].Location.Y + 3);
+                        Application.DoEvents();
+                    }
+                }
+            } catch (Exception ex)
+            {
+                moveObjects.Stop();
+                MessageBox.Show("Your computer cannot keep up with the load, application will now close.");
+                Environment.Exit(0);
+                return;
+            }
+        }
+    }
+}
